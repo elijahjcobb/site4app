398da8d6bfc9186c0285ddea54d553fd69a74496
Elijah Cobb <elijah@elijahcobb.com>
2023-02-19 01:46:34 -0500

    added og

--- components/client/page/index.tsx
+++ components/client/page/index.tsx
@@ -8,12 +8,16 @@ export function Page({
 	children
 }: PropsWithChildren) {
 
-	const { name, icon_small } = useApp();
+	const { name, icon_small, icon } = useApp();
 
 	return <div className={styles.page}>
 		<Head>
 			<title>{name}</title>
 			<link rel="icon" href={icon_small} />
+			<meta name='description' content={`View ${name} on site4.app.`} />
+			<meta property="og:image" content={`https://site4.app/api/og?app=${name}&icon=${icon}`} />
+			<meta property="og:title" content={name} />
+			<meta property="og:description" content={`View ${name} on site4.app.`} />
 		</Head>
 		<Nav />
 		<div className={styles.container}>
---
+++ pages/api/og.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from '@vercel/og';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+	runtime: 'edge',
+};
+
+// Make sure the font exists in the specified path:
+const font = fetch(new URL('../../assets/grotesk.ttf', import.meta.url)).then(
+	(res) => res.arrayBuffer(),
+);
+
+export default async function OG(req: NextRequest) {
+
+	const fontData = await font;
+
+	const { searchParams } = req.nextUrl;
+	const app = searchParams.get('app');
+	const icon = searchParams.get('icon');
+
+	if (!app || !icon) {
+		return NextResponse.json({ err: "Missing params." })
+	}
+
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					fontSize: 128,
+					background: 'white',
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					fontFamily: '"Space Grotesk"',
+					textAlign: 'center',
+					alignItems: 'center',
+					padding: 32,
+					justifyContent: 'center',
+					gap: 64,
+				}}
+			>
+				<img src={icon} style={{
+					width: 480,
+					height: 480,
+					borderRadius: "25%"
+				}} alt="icon" />
+				<span style={{
+					textAlign: "left",
+					fontSize: 128
+				}}>{app}</span>
+			</div>
+		),
+		{
+			width: 1200,
+			height: 600,
+			fonts: [
+				{
+					name: 'Typewriter',
+					data: fontData,
+					style: 'normal',
+				},
+			],
+		},
+	);
+}
