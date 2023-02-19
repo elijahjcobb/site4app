310f54276b81e90acde1a8c7a9da2f3988e8f565
Elijah Cobb <elijah@elijahcobb.com>
2023-02-19 01:13:42 -0500

    update icon and title

--- components/client/page/index.tsx
+++ components/client/page/index.tsx
@@ -1,11 +1,20 @@
 import { PropsWithChildren } from "react";
 import { Nav } from "../nav";
 import styles from "./index.module.css";
+import { useApp } from "../app-provider";
+import Head from "next/head";
 
 export function Page({
 	children
 }: PropsWithChildren) {
+
+	const { name, icon_small } = useApp();
+
 	return <div className={styles.page}>
+		<Head>
+			<title>{name}</title>
+			<link rel="icon" href={icon_small} />
+		</Head>
 		<Nav />
 		<div className={styles.container}>
 			{children}
